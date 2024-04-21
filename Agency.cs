e73111333badedab8c98e15a5ccb427e44358324
vasilgalev <156503882+vasilgalev@users.noreply.github.com>
2024-04-21 10:56:08 +0300

    Add files via upload

---
+++ Agency.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace RegularExamOOP
+{
+    public class Agency
+    {
+        private List<RealEstate> realEstates;
+
+        public Agency(string name)
+        {
+            //TODO: Delete the line and put your code here
+            throw new NotImplementedException();
+        }
+
+        private string name;
+        public string Name
+        {
+            //TODO: Delete the line and put your code here
+            throw new NotImplementedException();
+        }
+
+        public void AddRealEstate(RealEstate realEstate)
+        {
+            //TODO: Delete the line and put your code here
+            throw new NotImplementedException();
+        }
+
+        public bool SellRealEstate(RealEstate realEstate)
+        {
+            //TODO: Delete the line and put your code here
+            throw new NotImplementedException();
+        }
+
+        public double CalculateTotalPrice()
+        {
+            //TODO: Delete the line and put your code here
+            throw new NotImplementedException();
+        }
+
+        public RealEstate GetRealEstateWithHighestPrice()
+        {
+            //TODO: Delete the line and put your code here
+            throw new NotImplementedException();
+        }
+
+        public RealEstate GetRealEstateWithLowestPrice()
+        {
+            //TODO: Delete the line and put your code here
+            throw new NotImplementedException();
+        }
+
+        public void RenameAgency(string newName)
+        {
+            //TODO: Delete the line and put your code here
+            throw new NotImplementedException();
+        }
+
+        public void SellAllRealEstates()
+        {
+            //TODO: Delete the line and put your code here
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            //TODO: Delete the line and put your code here
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RealEstate.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RegularExamOOP
+{
+    public class RealEstate
+    {
+
+        public RealEstate(string address, double price)
+        {
+            //TODO: Delete the line and put your code here
+            throw new NotImplementedException();
+        }
+        
+        private string address;
+        public string Address
+        {
+            get
+            {
+                //TODO: Delete the line and put your code here
+                throw new NotImplementedException();
+            }
+            set
+            {
+                //TODO: Delete the line and put your code here
+                throw new NotImplementedException();
+            }
+        }
+
+        private double price;
+        public double Price
+        {
+            get
+            {
+                //TODO: Delete the line and put your code here
+                throw new NotImplementedException();
+            }
+            set
+            {
+                //TODO: Delete the line and put your code here
+                throw new NotImplementedException();
+            }
+        }
+
+        public override string ToString()
+        {    
+            //TODO: Delete the line and put your code here
+            throw new NotImplementedException();
+        }
+    }
+}
